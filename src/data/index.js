83ef865152c1346a68f692d8b1ed0f9461543b27
Sandhiya <148973818+S-Sandhiya-11@users.noreply.github.com>
2023-11-19 17:51:17 +0530

    Employee Management System
    Its Employee Manage software system .its id used to Update ,Add ,Delete and Modified.

---
+++ src/data/index.js
@@ -0,0 +1,13 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'SANDY',
+    lastName: 'Jordon',
+    email: 'sandy@gmail.com',
+    salary: '95000',
+    date: '2019-04-11'
+  }
+ 
+];
+
+export { employeesData };
